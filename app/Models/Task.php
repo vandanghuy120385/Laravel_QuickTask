ec67275ef1bcde31007838e18dd73fd0917c354a
vandanghuy120385 <van.dang.huy@sun-asterisk.com>
2023-08-24 18:03:33 +0700

    pr chuong 3

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'is_finished',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
