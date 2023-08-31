14bf0af004304d704679ad3787170442a03b5b22
vandanghuy120385 <van.dang.huy@sun-asterisk.com>
2023-08-31 09:53:10 +0700

    pr chuong 6

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::controller(TaskController::class)->group(function () {
+
+    Route::get('/tasks', 'index')->name('tasks.index');
+
+    Route::get('/tasks/create', 'create')->name('tasks.create');
+
+    Route::get('/tasks/{task}', 'show')->name('tasks.show');
+
+    Route::get('/tasks/edit', 'edit')->name('tasks.edit');
+
+    Route::post('/tasks', 'store')->name('tasks.store');
+
+    Route::put('/tasks/{task}', 'update')->name('tasks.update');
+
+    Route::delete('/tasks/{task}', 'destroy')->name('tasks.delete');
+});
+
+Route::resource('/user', UserController::class);
