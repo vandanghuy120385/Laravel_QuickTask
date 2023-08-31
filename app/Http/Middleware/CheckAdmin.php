c4e29c17dbbaca4c706e05f06d3ed658372bd278
vandanghuy120385 <van.dang.huy@sun-asterisk.com>
2023-08-31 11:30:02 +0700

    pr chuong 7

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+        if ($user && $user->is_admin) {
+            return $next($request);
+        }
+        abort(401);
+    }
+}
